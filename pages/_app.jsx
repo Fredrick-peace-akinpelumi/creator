fca9c856d2ada80e9ec8f7bb8df297f10781c61f
Fredrick-peace-akinpelumi <108526101+Fredrick-peace-akinpelumi@users.noreply.github.com>
2022-12-08 14:19:30 +0100

    Initial commit

---
+++ pages/_app.jsx
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import Form from '../component/Form';
+
+function MyApp({ Component, pageProps }) {
+  return<>
+  
+  <Component {...pageProps} />
+  </> 
+}
+
+export default MyApp
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import Form from '../component/Form'
+
+const index = () => {
+  return (
+    <Form/>
+  )
+}
+
+export default index
